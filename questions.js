0398dc77264ee7826307e6bac228e2e1e75ab822
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:18:14 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"___ she go to work every day?",
+        options:["Do", "Does", "Is", "Has"],
+        answer: 1
+    },
+    {
+     q:"___ you speak English?",
+        options:["Do", "Are", "Does", "Have"],
+        answer: 0   
+    },
+    {
+        q:"___ they often play football?",
+        options:["Are", "Does", "Do", "Have"],
+        answer: 2
+    },
+    {
+        q:"___ he like watching TV?",
+        options:["Do", "Has", "Is", "Does"],
+        answer: 3
+    },
+    {
+        q:"___ your mum like cooking?",
+        options:["Is", "Do", "Does", "Has"],
+        answer: 2
+    },
+    {
+        q:"How often ___ you go to the cinema?",
+        options:["have", "do", "does", "are"],
+        answer: 1
+    },
+    {
+        q:"What time ___ you usually wake up on Sunday?",
+        options:["do", "does", "is", "have"],
+        answer: 0
+    },
+    {
+        q:"___ your friend phone to you every day?",
+        options:["Do", "Has", "Is", "Does"],
+        answer: 3
+    },
+    {
+        q:"What ___ you usually drink in the morning?",
+        options:["are", "do", "does", "have"],
+        answer: 1
+    },
+    {
+        q:"___ your father have a car?",
+        options:["Is", "Do", "Does", "Has"],
+        answer: 2
+    }
+
+]
